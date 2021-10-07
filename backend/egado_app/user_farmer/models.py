28ce36fbdadb060368ec8cb76cb795d9b8333e45
Deivid Carvalho <190026758@aluno.unb.br>
2021-10-07 20:41:48 -0300

    Retirando refresh

--- backend/egado_app/user_farmer/models.py
+++ backend/egado_app/user_farmer/models.py
@@ -52,6 +52,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     def tokens(self):
         refresh=RefreshToken.for_user(self)
         return {
-            'refresh': str(refresh),
             'access': str(refresh.access_token)
         }
